e65ce6631250702646e0ce777fdbcd348d002d29
Janusz <janusz.aa@interia.pl>
2017-12-02 09:51:19 +0100

    asdss

--- src/zajecia3/Main.java
+++ src/zajecia3/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello Wdsdffaorld");
+        primaryStage.setTitle("Hello Wdassdffaorld");
         primaryStage.setScene(new Scene(root, 700, 500));
         primaryStage.show();
     }
